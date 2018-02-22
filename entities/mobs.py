af99f7416cab1cbeaf40224f557cd315b8eddd99
JarrodDoyle <jarrod.doyle@bourne-grammar.lincs.sch.uk>
2018-02-22 20:12:01 +0000

    Added inital monster stats and added to monster spawn rotation
    Monster stats need basic balancing.

--- entities/mobs.py
+++ entities/mobs.py
@@ -65,3 +65,233 @@ class Mob_Cockatrice(Entity):
         equipment_component = Equipment()
         components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
         super().__init__(x, y, "cockatrice", "c", "yellow", "Dangerous bird.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Wolf(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("wolf", strength = 4, defense = 2, accuracy = 3, intelligence = 0, dexterity = 0, max_hp = 25)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 3)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "wolf", "d", "brown", "A ferocious wolf.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Floating_Eye(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("floating eye", strength = 1, defense = 1, accuracy = 3, intelligence = 0, dexterity = 0, max_hp = 40)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 2)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "floating eye", "e", "green", "A strange floating eye.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Feral_Cat(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("feral cat", strength = 3, defense = 1, accuracy = 4, intelligence = 0, dexterity = 0, max_hp = 15)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 2)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "feral cat", "f", "orange", "A feral cat.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Gremlin(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("gremlin", strength = 6, defense = 3, accuracy = 3, intelligence = 0, dexterity = 0, max_hp = 20)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 4)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "gremlin", "d", "green", "A strange green gremlin", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Hobbit(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("hobbit", strength = 2, defense = 2, accuracy = 2, intelligence = 0, dexterity = 0, max_hp = 10)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 1)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "hobbit", "h", "green", "A short, weak humanoid.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Imp(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("imp", strength = 3, defense = 1, accuracy = 2, intelligence = 0, dexterity = 0, max_hp = 5)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 1)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "imp", "i", "red", "An extremely small and annoying demon.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Kobold(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("kobold", strength = 2, defense = 2, accuracy = 2, intelligence = 0, dexterity = 0, max_hp = 10)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 1)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "kobold", "k", "pink", "A weak pathetic being.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Nymph(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("nymph", strength = 5, defense = 3, accuracy = 5, intelligence = 0, dexterity = 0, max_hp = 30)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 4)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "nymph", "n", "blue", "An extremely dangerous creature.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Orc(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("orc", strength = 7, defense = 4, accuracy = 4, intelligence = 0, dexterity = 0, max_hp = 50)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 6)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "orc", "o", "red", "A large, strong orc.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Giant_Rat(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("giant rat", strength = 4, defense = 2, accuracy = 2, intelligence = 0, dexterity = 0, max_hp = 30)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 3)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "giant rat", "r", "grey", "A distgusting rat that is strangely large.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Cave_Spider(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("cave spider", strength = 2, defense = 1, accuracy = 1, intelligence = 0, dexterity = 0, max_hp = 5)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 1)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "cave spider", "s", "grey", "A common small spider.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Horse(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("horse", strength = 5, defense = 3, accuracy = 7, intelligence = 0, dexterity = 0, max_hp = 30)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 3)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "horse", "u", "grey", "A large warhorse.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Worm(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("worm", strength = 2, defense = 1, accuracy = 1, intelligence = 0, dexterity = 0, max_hp = 10)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 1)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "worm", "w", "pink", "A lorm squiggly worm.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Bat(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("bat", strength = 3, defense = 1, accuracy = 2, intelligence = 0, dexterity = 0, max_hp = 15)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 2)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "bat", "B", "red", "A large bat.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Centaur(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("centaur", strength = 7, defense = 4, accuracy = 5, intelligence = 0, dexterity = 0, max_hp = 30)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 5)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "centaur", "C", "cyan", "A half horse, half human creature.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Dragon(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("dragon", strength = 15, defense = 7, accuracy = 10, intelligence = 0, dexterity = 0, max_hp = 75)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 10)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "dragon", "D", "yellow", "An ancient dragon.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Gnome(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("gnome", strength = 5, defense = 3, accuracy = 4, intelligence = 0, dexterity = 0, max_hp = 40)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 4)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "gnome", "G", "brown", "A deceptively strong gnome.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Giant(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("giant", strength = 15, defense = 10, accuracy = 7, intelligence = 0, dexterity = 0, max_hp = 75)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 10)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "giant", "H", "yellow", "An extremely large, monsterous humanoid.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Lich(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("lich", strength = 10, defense = 5, accuracy = 5, intelligence = 0, dexterity = 0, max_hp = 50)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 7)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "lich", "L", "orange", "A magical lich.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Naga(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("naga", strength = 8, defense = 4, accuracy = 4, intelligence = 0, dexterity = 0, max_hp = 45)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 6)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "naga", "N", "red", "A monsterous being with the head of a human and the body of a snake.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Ogre(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("ogre", strength = 8, defense = 3, accuracy = 3, intelligence = 0, dexterity = 0, max_hp = 60)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 6)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "ogre", "O", "brown", "Strong humanoid monster.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Snake(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("snake", strength = 4, defense = 1, accuracy = 6, intelligence = 0, dexterity = 0, max_hp = 25)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 4)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "snake", "S", "green", "A slithering snake.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
+
+class Mob_Monkey(Entity):
+    def __init__(self, x, y):
+        fighter_class = Fighter_Class("monkey", strength = 3, defense = 2, accuracy = 3, intelligence = 0, dexterity = 0, max_hp = 30)
+        fighter_component = Fighter(fighter_class, spawn_chance = 100)
+        level_component = Level(base_level = 1, base_xp = 50, lvl_up_factor = 3, xp_drop = 2)
+        ai_component = Basic_Monster()
+        equipment_component = Equipment()
+        components_dict = {"ai": ai_component, "fighter": fighter_component, "level": level_component, "equipment": equipment_component}
+        super().__init__(x, y, "monkey", "M", "grey", "A small and weak monkey.", blocks = True, render_order = Render_Order.ACTOR, components = components_dict)
--- map_generation/spawn_entities.py
+++ map_generation/spawn_entities.py
@@ -5,7 +5,7 @@ from random import choice
 from inspect import getmembers, isclass
 
 def get_mob_list():
-    mob_list = [mobs.Mob_Goblin, mobs.Mob_Ant, mobs.Mob_Blob, mobs.Mob_Cockatrice]
+    mob_list = [mobs.Mob_Goblin, mobs.Mob_Ant, mobs.Mob_Blob, mobs.Mob_Cockatrice, mobs.Mob_Wolf, mobs.Mob_Floating_Eye, mobs.Mob_Feral_Cat, mobs.Mob_Gremlin, mobs.Mob_Hobbit, mobs.Mob_Imp, mobs.Mob_Kobold, mobs.Mob_Nymph, mobs.Mob_Orc, mobs.Mob_Giant_Rat, mobs.Mob_Cave_Spider, mobs.Mob_Horse, mobs.Mob_Worm, mobs.Mob_Bat, mobs.Mob_Centaur, mobs.Mob_Dragon, mobs.Mob_Gnome, mobs.Mob_Giant, mobs.Mob_Lich, mobs.Mob_Naga, mobs.Mob_Ogre, mobs.Mob_Snake, mobs.Mob_Monkey]
     #mob_list = [list(i[1][-len(i[0]):-3] for i in getmembers(mobs, isclass) if i[0][:3] == "Mob"]
     #mob_list = getmembers(mobs, isclass)
     #print(mob_list[0][0][:3])
@@ -32,4 +32,3 @@ def choose_mob_to_spawn():
         roll = dice.roll_dice(1, 100)[0]
         mob_spawned = [0,1][roll <= mob(0,0).components["fighter"].spawn_chance]
     return mob
-        
