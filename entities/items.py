27172627a8d2670924bd5761b1bdec81e3cb58e7
JarrodDoyle <jarrod.doyle@bourne-grammar.lincs.sch.uk>
2018-02-22 23:27:49 +0000

    Added spawning stat potions

--- entities/items.py
+++ entities/items.py
@@ -3,9 +3,51 @@ from entities.entity_classes import Entity
 from components.item import Item
 import use_functions
 
-class Health_Pot(Entity):
+class Health_Potion(Entity):
     def __init__(self, x, y):
         use_args = {"heal_amount": 30}
-        item_component = Item(use_function = use_functions.heal_entity, args = use_args, spawn_chance = 25)
+        item_component = Item(use_function = use_functions.heal_entity, args = use_args, spawn_chance = 100)
         components_dict = {"item": item_component}
-        super().__init__(x, y, "Health Pot", "!", "yellow", "A weak health potion. Restores health to whomever drinks it.", blocks = False, render_order = Render_Order.ITEM, components = components_dict)
+        super().__init__(x, y, "Health Potion", "!", "yellow", "A weak health potion. Restores health to whomever drinks it.", blocks = False, render_order = Render_Order.ITEM, components = components_dict)
+
+class Strength_Potion(Entity):
+    def __init__(self, x, y):
+        use_args = {"strength_increase": 2}
+        item_component = Item(use_function = use_functions.increase_strength, args = use_args, spawn_chance = 100)
+        components_dict = {"item": item_component}
+        super().__init__(x, y, "Strength Potion", "!", "yellow", "A strength potion. Gives increased strength to whomever drinks it.", blocks = False, render_order = Render_Order.ITEM, components = components_dict)
+
+class Defense_Potion(Entity):
+    def __init__(self, x, y):
+        use_args = {"defense_increase": 2}
+        item_component = Item(use_function = use_functions.increase_defense, args = use_args, spawn_chance = 100)
+        components_dict = {"item": item_component}
+        super().__init__(x, y, "Defense Potion", "!", "yellow", "A defense potion. Gives increased defense to whomever drinks it.", blocks = False, render_order = Render_Order.ITEM, components = components_dict)
+
+class Accuracy_Potion(Entity):
+    def __init__(self, x, y):
+        use_args = {"accuracy_increase": 2}
+        item_component = Item(use_function = use_functions.increase_accuracy, args = use_args, spawn_chance = 100)
+        components_dict = {"item": item_component}
+        super().__init__(x, y, "Accuracy Potion", "!", "yellow", "A accuracy potion. Gives increased accuracy to whomever drinks it.", blocks = False, render_order = Render_Order.ITEM, components = components_dict)
+
+class Intelligence_Potion(Entity):
+    def __init__(self, x, y):
+        use_args = {"intelligence_increase": 2}
+        item_component = Item(use_function = use_functions.increase_intelligence, args = use_args, spawn_chance = 100)
+        components_dict = {"item": item_component}
+        super().__init__(x, y, "Intelligence Potion", "!", "yellow", "A intelligence potion. Gives increased intelligence to whomever drinks it.", blocks = False, render_order = Render_Order.ITEM, components = components_dict)
+
+class Dexterity_Potion(Entity):
+    def __init__(self, x, y):
+        use_args = {"dexterity_increase": 2}
+        item_component = Item(use_function = use_functions.increase_dexterity, args = use_args, spawn_chance = 25)
+        components_dict = {"item": item_component}
+        super().__init__(x, y, "Dexterity Potion", "!", "yellow", "A dexterity potion. Gives increased dexterity to whomever drinks it.", blocks = False, render_order = Render_Order.ITEM, components = components_dict)
+
+class Max_HP_Potion(Entity):
+    def __init__(self, x, y):
+        use_args = {"max_hp_increase": 2}
+        item_component = Item(use_function = use_functions.increase_max_hp, args = use_args, spawn_chance = 25)
+        components_dict = {"item": item_component}
+        super().__init__(x, y, "Max HP Potion", "!", "yellow", "A max HP potion. Gives increased max HP to whomever drinks it.", blocks = False, render_order = Render_Order.ITEM, components = components_dict)
--- use_functions.py
+++ use_functions.py
@@ -1,6 +1,6 @@
 def heal_entity(entity, args):
     heal_amount = args.get("heal_amount")
-    if entity.components.get("fighter") and entity.components["fighter"].hp < entity.components["fighter"].max_hp:
+    if heal_amount is not None and entity.components.get("fighter") and entity.components["fighter"].hp < entity.components["fighter"].max_hp:
         fighter = entity.components["fighter"]
         fighter.hp += heal_amount
         if fighter.hp > fighter.max_hp:
@@ -12,3 +12,57 @@ def heal_entity(entity, args):
 def damage_entity(entity, args):
     args = {"heal_amount": args.get("damage_amount")}
     return heal_entity(entity, args)
+
+def increase_strength(entity, args):
+    increase_amount = args.get("strength_increase")
+    if increase_amount is not None and entity.components.get("fighter"):
+        fighter = entity.components["fighter"]
+        fighter.strength += increase_amount
+        return True
+    else:
+        return False
+
+def increase_defense(entity, args):
+    increase_amount = args.get("defense_increase")
+    if increase_amount is not None and entity.components.get("fighter"):
+        fighter = entity.components["fighter"]
+        fighter.defense += increase_amount
+        return True
+    else:
+        return False
+
+def increase_accuracy(entity, args):
+    increase_amount = args.get("accuracy_increase")
+    if increase_amount is not None and entity.components.get("fighter"):
+        fighter = entity.components["fighter"]
+        fighter.accuracy += increase_amount
+        return True
+    else:
+        return False
+
+def increase_dexterity(entity, args):
+    increase_amount = args.get("dexterity_increase")
+    if increase_amount is not None and entity.components.get("fighter"):
+        fighter = entity.components["fighter"]
+        fighter.dexterity += increase_amount
+        return True
+    else:
+        return False
+
+def increase_intelligence(entity, args):
+    increase_amount = args.get("intelligence_increase")
+    if increase_amount is not None and entity.components.get("fighter"):
+        fighter = entity.components["fighter"]
+        fighter.intelligence += increase_amount
+        return True
+    else:
+        return False
+
+def increase_max_hp(entity, args):
+    increase_amount = args.get("max_hp_increase")
+    if increase_amount is not None and entity.components.get("fighter"):
+        fighter = entity.components["fighter"]
+        fighter.max_hp += increase_amount
+        return True
+    else:
+        return False
