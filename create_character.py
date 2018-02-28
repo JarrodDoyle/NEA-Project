f4bb2b02c1780d0fca8ef6873f75f719a03bae6f
JarrodDoyle <jarrod.doyle@bourne-grammar.lincs.sch.uk>
2018-02-28 17:36:06 +0000

    Updated character creation

--- create_character.py
+++ create_character.py
@@ -1,20 +1,26 @@
 from bearlibterminal import terminal
 from ui import Generic_Text_Window
 import components.fighter as fighter
+import re
 
 class Character_Creation:
     def choose_name(self):
         name = ""
         menu = Generic_Text_Window(1, 1, 94, 62, "Character Creation")
+        invalid_text = ""
         while True:
             terminal.clear()
-            text = "Please enter your name:\n{}".format(name)
+            text = "Please enter your name:\n{}\n{}".format(name, invalid_text)
             menu.render(text)
             terminal.refresh()
+            invalid_text = ""
             key = terminal.read()
 
-            if (key == terminal.TK_RETURN or key == terminal.TK_ESCAPE) and len(name) > 0:
-                break
+            if (key == terminal.TK_RETURN or key == terminal.TK_ESCAPE):
+                if re.match("^[A-Za-z]+$", name) and len(name) > 0:
+                    break
+                else:
+                    invalid_text = "That name is not valid."
             elif key == terminal.TK_BACKSPACE and len(name) > 0:
                 name = name[:-1]
             elif terminal.check(terminal.TK_CHAR) and len(name) < 8:
